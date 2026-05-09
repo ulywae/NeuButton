6ca3bbd3f1b77bcc3a4ad4ce51d013200f3d6c4a
Neufa <43496523+ulywae@users.noreply.github.com>
2026-05-09 08:09:48 +0700

    Update LongPress.ino

--- examples/LongPress/LongPress.ino
+++ examples/LongPress/LongPress.ino
@@ -1,28 +1,26 @@
 #include <NeuButton.h>
 
 const uint8_t pins[] = {2, 3};
-const uint8_t numPins = sizeof(pins) / sizeof(pins[0]);
+constexpr uint8_t numPins = sizeof(pins) / sizeof(pins[0]);
 
-NeuButton btn(pins, numPins);
+NeuButton<numPins> btn(pins);
 
 void setup()
 {
     Serial.begin(115200);
 
     // Long press on button 0 (2 seconds)
-    btn.addLongPress(0, 2000, [](uint8_t i) {
-        Serial.println("Button 0 LONG PRESS!");
-    });
+    btn.addLongPress(0, 2000, [](uint8_t i)
+                     { Serial.println("Button 0 LONG PRESS!"); });
 
     // Repeat on button 1 (every 300ms)
-    btn.addRepeat(1, 300, [](uint8_t i) {
-        Serial.println("Button 1 REPEATING...");
-    });
+    btn.addRepeat(1, 300, [](uint8_t i)
+                  { Serial.println("Button 1 REPEATING..."); });
 
-    btn.onPressed([](uint8_t i) {
+    btn.onPressed([](uint8_t i)
+                  {
         Serial.print("Pressed: ");
-        Serial.println(i);
-    });
+        Serial.println(i); });
 
     btn.refresh();
 }
