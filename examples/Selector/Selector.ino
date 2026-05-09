13430dc3cf904156ee5138cc892e102781a2a3ff
Neufa <43496523+ulywae@users.noreply.github.com>
2026-05-09 08:10:32 +0700

    Update Selector.ino

--- examples/Selector/Selector.ino
+++ examples/Selector/Selector.ino
@@ -1,9 +1,9 @@
 #include <NeuButton.h>
 
 const uint8_t pins[] = {2, 3, 4};
-const uint8_t numPins = sizeof(pins) / sizeof(pins[0]);
+constexpr uint8_t numPins = sizeof(pins) / sizeof(pins[0]);
 
-NeuButton btn(pins, numPins);
+NeuButton<numPins> btn(pins);
 
 void setup()
 {
@@ -12,13 +12,13 @@ void setup()
     // Enable exclusive latch (radio-style selector)
     btn.setExclusiveLatch(true);
 
-    btn.onLatch([](uint8_t i, bool state) {
+    btn.onLatch([](uint8_t i, bool state)
+                {
         if (state)
         {
             Serial.print("Selected Mode: ");
             Serial.println(i);
-        }
-    });
+        } });
 
     // Sync initial state (important for selector switches)
     btn.refresh();
